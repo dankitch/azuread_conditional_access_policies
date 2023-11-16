1e7ee7500795b3dd6ec826f4fdd4425e3fdfed67
Dan Kitchen <140333496+dankitch@users.noreply.github.com>
2023-11-16 20:29:24 +0000

    Update main.tf

--- policies/main.tf
+++ policies/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">=1.6.2"
-
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "2.44.1"
-    }
-  }
-  backend "azurerm" {
-  }
-
-}
-
-provider "azuread" {
-}
-
-
 locals {
   policy1 = {
     sequence_number  = "CA001"

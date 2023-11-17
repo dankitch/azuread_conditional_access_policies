248525fdcf8d36122725434d7884674a4bc5f6f9
Dan Kitchen <140333496+dankitch@users.noreply.github.com>
2023-11-17 12:29:32 +0000

    code review changes

--- policies/CA002.tf
+++ policies/CA002.tf
@@ -1,30 +1,24 @@
 resource "azuread_conditional_access_policy" "CA002" {
-  display_name = "${var.tenant} - ${local.policy2.sequence_number} - ${local.policy2.personas_groups} - ${local.policy2.cloud_app_target} : ${local.policy2.response}"
-  state        = var.Report-only // Report-only
+  display_name = "${var.tenant} - CA002 - Test-RING - User Sign-in Risk (High) - All Cloud Apps : Block"
+  state        = var.report_only
 
   conditions {
-    client_app_types = ["all"]
-    // service_principal_risk_levels = []
+    client_app_types    = ["all"]
     sign_in_risk_levels = ["high"]
-    // user_risk_levels              = []
+
     applications {
-      // excluded_applications = []
       included_applications = ["All"]
-      // included_user_actions = []
     }
+
     users {
-      // excluded_groups = []
-      // excluded_roles  = []
-      excluded_users  = [var.break-glass]
-      included_groups = [var.CA002-Test-RING]
-      // included_roles  = []
-      // included_users  = []
+      excluded_users  = [var.break_glass]
+      included_groups = [var.ca002_test_ring]
     }
+
   }
   grant_controls {
     built_in_controls = ["block"]
-    // custom_authentication_factors = []
-    operator = "OR"
-    // terms_of_use                  = []
+    operator          = "OR"
   }
+
 }
--- policies/CA006.tf
+++ policies/CA006.tf
@@ -1,32 +1,26 @@
 resource "azuread_conditional_access_policy" "CA006" {
-  display_name = "${var.tenant} - ${local.policy6.sequence_number} - ${local.policy6.personas_groups} - ${local.policy6.cloud_app_target} : ${local.policy6.response}"
-  state        = var.Enabled
+  display_name = "${var.tenant} - CA006 - iOS + Android - Mobile Apps - Office 365 : Require Approved App OR App Protection Policy"
+  state        = var.enabled
+
   conditions {
     client_app_types = ["mobileAppsAndDesktopClients"]
-    // sign_in_risk_levels = []
-    // user_risk_levels    = []
+
     applications {
-      // excluded_applications = []
       included_applications = ["Office365"]
-      // included_user_actions = []
     }
+
     platforms {
-      // excluded_platforms = []
       included_platforms = ["android", "iOS", "windowsPhone"]
     }
+
     users {
-      // excluded_groups = []
-      // excluded_roles  = []
-      // excluded_users  = []
-      included_groups = [var.pol-apply-user-AllowBYoDAccess]
-      // included_roles  = []
-      // included_users  = []
+      included_groups = [var.pol_apply_user_allow_byod_access]
     }
   }
+
   grant_controls {
     built_in_controls = ["approvedApplication", "compliantApplication"]
-    // custom_authentication_factors = []
-    operator = "OR"
-    // terms_of_use                  = []
+    operator          = "OR"
   }
+
 }
